a89d0d148c70d0347d593bfaf4cd75548066648b
Thomas Cilano <Tom@Thomass-MacBook-Pro.local>
2016-09-07 14:24:08 -0400

    FixIt

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Tom on 9/7/16.
+ */
+public class Main {
+
+    private static int x = 12;
+    private static int y = 28;
+    private static double z = 3.3;
+
+    public static void main(String[] args) {
+        System.out.println("The answer to(x * y) / 3.3 is");calculate();
+
+    }
+
+    private static double calculate() {
+        double Answer = (x * y) / z;
+        System.out.println(Answer);
+        return Answer;
+
+final
+
+    }
+}
